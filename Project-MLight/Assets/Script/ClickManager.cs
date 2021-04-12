ade976b57d0bcc0fd330f82569d5cfbf0e1e30b2
Eggbob <dhm04155@naver.com>
2021-04-12 23:26:08 +0900

    플레이어 이동

---
+++ Project-MLight/Assets/Script/ClickManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+    [SerializeField]
+    private Animator anim;
+    private NavMeshAgent nav;
+
+    private bool isMove;
+    private Vector3 destination;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+        anim = GetComponentInChildren<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        nav.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition),out hit ))
+            {
+                SetDestination(hit.point);
+            }
+        }
+        Move();
+    }
+
+    void SetDestination(Vector3 dest)
+    {
+        nav.SetDestination(dest);
+        destination = dest;
+        isMove = true;
+        anim.SetBool("isRun", true);
+       
+    }
+
+    private void Move()
+    {
+        if(isMove)
+        {
+            if(nav.velocity.magnitude == 0f)
+            {
+                isMove = false;
+                anim.SetBool("isRun", false);
+                return;
+            }
+
+            var dir = new Vector3(nav.steeringTarget.x, transform.position.y, nav.steeringTarget.z)
+                - transform.position;
+
+            anim.transform.forward = dir;
+        
+        }
+
+       
+    }
+}
---
+++ Project-MLight/Assets/Script/MainCameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraFollow : MonoBehaviour
+{
+    public Transform target; // 따라다닐 대상 트랜스폼 컴포넌트
+    public Vector3 offset; // 카메라 위치
+
+    private void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
