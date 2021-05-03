a5f86775f51eec67246e3fd61480eb876f3212d6
Eggbob <dhm04155@naver.com>
2021-05-04 01:47:00 +0900

    0504

---
+++ Project-MLight/Assets/Script/PublicScript/Skill/ActiveSkill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ActiveSkill : Skill
+{
+    public int MpCost; //마나 사용량
+    public float coolTime; // 재사용 대기시간
+    public event Action active;
+
+    private void Start()
+    {
+        active += ActiveAction;
+    }
+
+    private  void ActiveAction()
+    {
+
+    }
+
+
+
+}
--- Project-MLight/Assets/Script/PublicScript/Skill/Skill.cs
+++ Project-MLight/Assets/Script/PublicScript/Skill/Skill.cs
@@ -2,19 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Skill : MonoBehaviour
 {
     public Texture Icon; // 스킬 아이콘
     public string SkillName; //스킬 이름
     public string Description; //스킬 설명
     public int Skillid; //스킬 아이디
-
-    public Skill(Skill s)
-    {
-        Icon = s.Icon;
-        name = s.name;
-        Description = s.Description;
-        Skillid = s.Skillid;
-    }
+    public float SkillExp; //스킬 진척도
 
 }
