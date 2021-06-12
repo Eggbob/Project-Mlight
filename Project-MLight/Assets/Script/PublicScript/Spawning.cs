c82d8e318910f1f105f43c6cc054f637052ab327
Eggbob <dhm04155@naver.com>
2021-06-12 11:51:10 +0900

    스폰 추가

---
+++ Project-MLight/Assets/Script/PublicScript/Spawning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject GobeHunter;
+    public GameObject Gobe;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            Vector3 spawnPos = this.transform.position;
+            spawnPos.y += 1f;
+            Instantiate(GobeHunter, spawnPos, Quaternion.identity);
+        }
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            Vector3 spawnPos = this.transform.position;
+            spawnPos.y += 1f;
+            Instantiate(Gobe, spawnPos, Quaternion.identity);
+        }
+
+    }
+}
