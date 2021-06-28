7726e7df4bb97a8b06652173e29696b1c2d497c5
Eggbob <dhm04155@naver.com>
2021-06-28 13:56:27 +0900

    머지

---
+++ Project-MLight/Assets/Script/PublicScript/ItemData/PropItemData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Prop_Item", menuName = "Inventory System/Item Data/PropItem", order = 4)]
+public class PropItemData : CountableItemData
+{
+    public override Item CreateItem()
+    {
+        return new PropItem(this);
+    }
+}
---
+++ Project-MLight/Assets/Script/PublicScript/Items/PropItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropItem : CountableItem
+{
+    public PropItem(PropItemData data, int amount = 1) : base(data, amount) { }
+
+    protected override CountableItem Clone(int amount)
+    {
+        return new PortionItem(CountableData as PortionItemData, amount);
+    }
+}
